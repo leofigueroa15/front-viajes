5f3381fd3ee73d55e0533a0b07331f7a36cec1f5
leofigueroa15 <leofigueroa15@gmail.com>
2024-08-21 19:18:41 -0400

    se reformula componente acordeon

--- src/components/CarouselCard.jsx
+++ src/components/CarouselCard.jsx
@@ -1,38 +1,66 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
 
 const CarouselCard = () => {
     return (
-        <div className='container-xl mt-5'>
-            <div className="row justify-content-center justify-content-md-between">
-                <div style={{ width: '18rem' }} className='ml-3 card'>
-                    <img src="./public/img/roma.png" className="card-img-top" />
-                    <div className="card-body text-center">
-                        <a href="#" className="btn btn-primary">Roma</a>
-                    </div>
-                </div>
 
-                <div style={{ width: '18rem' }} className='ml-3 card'>
-                    <img src="./public/img/roma.png" className="card-img-top" />
-                    <div className="card-body text-center">
-                        <a href="#" className="btn btn-primary">Roma</a>
-                    </div>
-                </div>
+        <>
+            <Carousel className='mt-5' style={{ marginLeft: '5rem' }}>
+                <Carousel.Item interval={10000}>
+                    <CardGroup>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="./public/img/roma.png" />
+                            <Card.Body>
+                                <Button variant="primary">Roma</Button>
+                            </Card.Body>
 
-                <div style={{ width: '18rem' }} className='ml-3 card'>
-                    <img src="./public/img/roma.png" className="card-img-top" />
-                    <div className="card-body text-center">
-                        <a href="#" className="btn btn-primary">Roma</a>
-                    </div>
-                </div>
+                        </Card>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="./public/img/roma.png" />
+                            <Card.Body>
+                                <Button variant="primary">Roma</Button>
+                            </Card.Body>
 
-                <div style={{ width: '18rem' }} className='ml-3 card'>
-                    <img src="./public/img/roma.png" className="card-img-top" />
-                    <div className="card-body text-center">
-                        <a href="#" className="btn btn-primary">Roma</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+                        </Card>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="./public/img/roma.png" />
+                            <Card.Body>
+                                <Button variant="primary">Roma</Button>
+                            </Card.Body>
+
+                        </Card>
+                    </CardGroup>
+                </Carousel.Item>
+                <Carousel.Item interval={10000}>
+                    <CardGroup>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="./public/img/roma.png" />
+                            <Card.Body>
+                                <Button variant="primary">Roma</Button>
+                            </Card.Body>
+
+                        </Card>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="./public/img/roma.png" />
+                            <Card.Body>
+                                <Button variant="primary">Roma</Button>
+                            </Card.Body>
+
+                        </Card>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="./public/img/roma.png" />
+                            <Card.Body>
+                                <Button variant="primary">Roma</Button>
+                            </Card.Body>
+
+                        </Card>
+                    </CardGroup>
+                </Carousel.Item>
+            </Carousel>
+        </>
     )
 }
 
