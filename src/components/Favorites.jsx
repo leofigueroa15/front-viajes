c39e4543a026d5274975c7084e4bd58f1a9bf07f
leofigueroa15 <leofigueroa15@gmail.com>
2024-08-22 19:08:47 -0400

    se reformula componente favoritos

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,39 +1,61 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 
 const Favorites = () => {
     return (
-        <div className='container-xl mt-5'>
-            <h3 className='text-center mb-5'>Favoritos</h3>
 
-            <div className="row justify-content-center justify-content-md-between">
-                <div style={{ width: '18rem' }} className='ml-3 card'>
-                    <img src="./public/img/roma.png" className="card-img-top" />
-                    <div className="card-body">
-                        <h5 className="card-title">Ciudad</h5>
-                        <p className="card-text">Opiniones</p>
-                        <p className="card-text">Apertura</p>
-                    </div>
-                </div>
-
-                <div style={{ width: '18rem' }} className='ml-3 card'>
-                    <img src="./public/img/roma.png" className="card-img-top" />
-                    <div className="card-body">
-                        <h5 className="card-title">Ciudad</h5>
-                        <p className="card-text">Opiniones</p>
-                        <p className="card-text">Apertura</p>
-                    </div>
-                </div>
-
-                <div style={{ width: '18rem' }} className='ml-3 card'>
-                    <img src="./public/img/roma.png" className="card-img-top" />
-                    <div className="card-body">
-                        <h5 className="card-title">Ciudad</h5>
-                        <p className="card-text">Opiniones</p>
-                        <p className="card-text">Apertura</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Container className='mt-5'>
+            <Row>
+                <Col>
+                    <h3 className='text-center mb-5'>Favoritos</h3>
+                </Col>
+            </Row>
+            <Row>
+                <Card style={{ width: '18rem', marginLeft: '2rem' }}>
+                    <Card.Img variant="top" src="./public/img/roma.png" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button style={{color: 'black'}} variant="link">Link</Button>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem', marginLeft: '13rem' }}>
+                    <Card.Img variant="top" src="./public/img/roma.png" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button style={{color: 'black'}} variant="link">Link</Button>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem', marginLeft: '10rem' }}>
+                    <Card.Img variant="top" src="./public/img/roma.png" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button style={{color: 'black'}} variant="link">Link</Button>
+                    </Card.Body>
+                </Card>
+            </Row>
+            <Row>
+                <Col>
+                    <Button style={{color: 'black', marginLeft: '35rem'}} className='ml-5 mt-5' variant="link">"Ver mas favoritos"</Button>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
