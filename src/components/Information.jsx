e050ff410b3fcdbbccf77579d77ee645441ecf26
leofigueroa15 <leofigueroa15@gmail.com>
2024-08-22 18:23:26 -0400

    se reformula componente information

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,41 +1,59 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import FormLabel from 'react-bootstrap/esm/FormLabel';
 
 const Information = () => {
     return (
-
-        
         <>
-            <div className="navbar navbar-light bg-primary box-shadow mt-5 mb-0">
-                <div className="container d-flex justify-content-between text-white py-4">
-                    <div className="col-md-4 mx-auto text-left mb-2">
-                        <div className="media">
-                            <i className="fas fa-chart-line" style={{ fontSize: '2.45rem' , marginRight: '10%' , marginTop: '0.2rem'  }}></i>
-                            <div className="media-body" style={{fontSize: '16px'}}>
-                                <h6 className="mt-0 text-white"><u>INCREMENTA TUS VENTAS</u></h6>
-                                Gracias a nuestro sistema de reserva, podras mejorar tu desempeño y verlo reflejado en las ganacias de tu negocio.
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4 mx-auto text-left mb-2">
-                        <div className="media">
-                            <i className="bi bi-apple" style={{ fontSize: '1.9rem' , marginRight: '10%' , marginTop: '0.5rem'  }}></i>
-                            <div className="media-body" style={{fontSize: '16px'}}>
-                                <h6 className="mt-0 text-white"><u>EXPANDE TU PRESENCIA ONLINE</u></h6>
-                                Podras usar nuestros servicios desde tu portatil e interconectarla con tu propio site web y asi expandir tu presencia.
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4 mx-auto text-left mb-2">
-                        <div className="media">
-                            <i className="fas fa-globe" style={{ fontSize: '2.1rem' , marginRight: '10%' , marginTop: '0.4rem'  }}></i>
-                            <div className="media-body" style={{fontSize: '16px'}}>
-                                <h6 className="mt-0 text-white"><u> APUNTA A UN MAYOR MERCADO</u></h6>
-                                Tu presencia en nuestra web, aumenta el alcance de mercado para tus productos y te proporciona frente a la competencia.
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Navbar bg="dark" data-bs-theme="dark" className='mt-5 mb-0'>
+                <Container>
+                    <Nav>
+                        <Navbar.Text>
+                            <Row>
+                                <Col>
+                                    <Image src="./public/img/logoReservalo.png" roundedCircle />
+                                </Col>
+                                <Col>
+                                    <FormLabel style={{ display: 'block' }}>
+                                        EXPANDE TU PRESENCIA ONLINE
+                                    </FormLabel>
+                                    <FormLabel>
+                                        Gracias a nuestro sistema de reserva, podras mejorar tu desempeño y verlo reflejado en las ganacias de tu negocio.
+                                    </FormLabel>
+                                </Col>
+                                <Col>
+                                    <Image src="./public/img/logoReservalo.png" roundedCircle />
+                                </Col>
+                                <Col>
+                                    <FormLabel style={{ display: 'block' }}>
+                                        EXPANDE TU PRESENCIA ONLINE
+                                    </FormLabel>
+                                    <FormLabel>
+                                        Gracias a nuestro sistema de reserva, podras mejorar tu desempeño y verlo reflejado en las ganacias de tu negocio.
+                                    </FormLabel>
+                                </Col>
+                                <Col>
+                                    <Image src="./public/img/logoReservalo.png" roundedCircle />
+                                </Col>
+                                <Col >
+                                    <FormLabel style={{ display: 'block' }}>
+                                        EXPANDE TU PRESENCIA ONLINE
+                                    </FormLabel>
+                                    <FormLabel>
+                                        Gracias a nuestro sistema de reserva, podras mejorar tu desempeño y verlo reflejado en las ganacias de tu negocio.
+                                    </FormLabel>
+                                </Col>
+                            </Row>
+                        </Navbar.Text>
+
+                    </Nav>
+                </Container>
+            </Navbar>
         </>
     )
 }
